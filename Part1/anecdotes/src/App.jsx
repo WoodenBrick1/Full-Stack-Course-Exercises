8e12da90af85b25888b90e16a5fd8fe40d59786d
WoodenBrick1 <brickwooden@gmail.com>
2025-12-19 11:43:07 +0100

    adecdote exercises complete

---
+++ Part1/anecdotes/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+
+const Button = ({ onClick, name }) => <button onClick={onClick}>{name}</button>
+
+const DisplayVotes = ({ votes, selected }) => {
+  if (votes[selected] == null)
+    return (<p>Has 0 votes</p>)
+  return (<p>Has {votes[selected]} votes</p>)
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState({})
+  const [mostVotes, setMostVotes] = useState(0)
+
+  const voteAnecdote = () => {
+    const newVotes = { ...votes };
+
+    const selectedVotes = newVotes[selected];
+    const mostVotesNumber = newVotes[mostVotes];
+    if (selectedVotes == null)
+      newVotes[selected] = 0
+
+    newVotes[selected] += 1
+
+    if (mostVotesNumber != null && selectedVotes >= mostVotesNumber) {
+      setMostVotes(selected)
+    }
+    setVotes(newVotes);
+  }
+
+  const newAnectode = () => {
+    const index = Math.floor(Math.random() * anecdotes.length)
+    setSelected(index)
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <DisplayVotes votes={votes} selected={selected} />
+      <br />
+      <Button onClick={voteAnecdote} name="vote" />
+      <Button onClick={newAnectode} name="next anecdote" />
+
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[mostVotes]}
+      <DisplayVotes votes={votes} selected={mostVotes} />
+    </div>
+  )
+}
+
+export default App
