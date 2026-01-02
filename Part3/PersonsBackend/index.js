7e07ec9254022adeec1a80bf5b352f878ee82c23
WoodenBrick1 <brickwooden@gmail.com>
2026-01-02 17:11:50 +0100

    uibuild

--- Part3/PersonsBackend/index.js
+++ Part3/PersonsBackend/index.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-app.use(cors())
 
 morgan.token('post', (req, res) => {
     return JSON.stringify(req.body)
