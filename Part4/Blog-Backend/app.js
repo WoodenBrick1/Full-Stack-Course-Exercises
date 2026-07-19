3fd006b20e4ce1e3e9a361d2f287f2ec7069deea
WoodenBrick1 <brickwooden@gmail.com>
2026-07-19 20:26:21 +0300

    Exercise 4.2

---
+++ Part4/Blog-Backend/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+
+require( 'dotenv' ).config()
+
+const app = express()
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl, { family: 4 })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+
+module.exports = app
