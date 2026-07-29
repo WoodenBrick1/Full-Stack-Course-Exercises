9646a07ba5884c55304e8c1a35071e8627d396a1
WoodenBrick1 <brickwooden@gmail.com>
2026-07-29 13:51:25 +0300

    Exercise 4.19

--- Part4/Blog-Backend/controllers/blogs.js
+++ Part4/Blog-Backend/controllers/blogs.js
@@ -1,7 +1,19 @@
+const jwt = require('jsonwebtoken')
+
 const blogsRouter = require('express').Router()
 const { request } = require('express')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
+
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
@@ -20,12 +32,16 @@ blogsRouter.post('/', async (request, response) => {
         return response.status(400).end()
     }
 
-    const user = await User.findOne()
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
 
     if (!user) {
         return response.status(400).json({ error: 'userId missing or not valid' })
     }
-
 
     const blog = new Blog({
         title: body.title,
