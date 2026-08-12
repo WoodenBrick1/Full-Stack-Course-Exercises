a9e8b5ac9bc314b89d5fa641345d7e1b379929cd
WoodenBrick1 <brickwooden@gmail.com>
2026-08-12 14:47:32 +0300

    Exercise 5.2

--- Part5/bloglist-frontend/src/App.jsx
+++ Part5/bloglist-frontend/src/App.jsx
@@ -18,21 +18,35 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const userJson = window.localStorage.getItem('loginBlogUser')
+
+    if (userJson) {
+      setUser(JSON.parse(userJson))
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
     const user = await loginService.login({ username, password })
 
+    window.localStorage.setItem('loginBlogUser', JSON.stringify(user))
     setUser(user)
     setUsername('')
     setPassword('')
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loginBlogUser')
+    setUser(null)
   }
 
   return (
     <div>
 
       {!user && <LoginForm handleLogin={handleLogin} username={username} setUsername={setUsername} password={password} setPassword={setPassword} />}
-      {user && <Blogs blogs={blogs} />}
+      {user && <Blogs blogs={blogs} handleLogout={handleLogout} />}
 
     </div>
   )
--- Part5/bloglist-frontend/src/components/Blogs.jsx
+++ Part5/bloglist-frontend/src/components/Blogs.jsx
@@ -1,9 +1,10 @@
 import Blog from './Blog'
 
-const Blogs = ({ blogs }) => {
+const Blogs = ({ blogs, handleLogout }) => {
 
     return (
         <div>
+            <button onClick={handleLogout}>Logout</button>
             <h2>blogs</h2>
             {blogs.map(blog =>
                 <Blog key={blog.id} blog={blog} />
